33d43c5351e1e8ac01e36d4a6b093ee1c5f0d960
Alexander Shatravka <alexander.shatravka@gmail.com>
2022-02-21 15:22:01 +0200

    remove keys

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,11 +1,3 @@
 export default {
-  credentials: {
-    apiKey: "AIzaSyCGM6NkyEtfY5BLiFPMHacQNdAioQRR3qY",
-    authDomain: "lagerscan-app.firebaseapp.com",
-    projectId: "lagerscan-app",
-    storageBucket: "lagerscan-app.appspot.com",
-    messagingSenderId: "364285253553",
-    appId: "1:364285253553:web:12b7baa877fdadab778f90",
-    measurementId: "G-SNLHSXRCVL",
-  }
+  credentials: {}
 }
